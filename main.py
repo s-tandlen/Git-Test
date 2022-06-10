d4c3c5e98db03df9d31ad7e8592ce317abafe867
Hofilenab <107134769+Hofilenab@users.noreply.github.com>
2022-06-10 16:29:16 +0000

    Fixed if statements

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 name=input("What is your name: ")
 print("Hi "+name)
-feelings= input("How are you today, good, bad, or meh   ")
-if feelings: "good"
-print("yay")
-if feelings: "bad"
-print("oh no")
-if feelings: "meh"
-print("that's better than it being bad")
+feelings= input("How are you today, good, bad, or meh: ")
+if feelings=="good":
+  print("yay")
+else:
+  if feelings=="bad":
+    print("oh no")
+  else:
+    if feelings=="meh":
+      print("that's better than it being bad")
