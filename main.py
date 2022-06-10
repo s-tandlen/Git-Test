87fa96baf88a6d26a9dd7becf459d96a7a884b12
tandlen <107134774+s-tandlen@users.noreply.github.com>
2022-06-10 16:24:55 +0000

    asked feeling and if then statements

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 name=input("What is your name: ")
 print("Hi "+name)
+feelings= input("How are you today, good, bad, or meh   ")
+if feelings: "good"
+print("yay")
+if feelings: "bad"
+print("oh no")
+if feelings: "meh"
+print("that's better than it being bad")
